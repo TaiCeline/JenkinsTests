fa229f940583f596ba63d4aea8110e6c73cd39f6
Celine <naughty8212@gmail.com>
2020-07-08 19:44:58 +0800

    Added Test project

---
+++ JenkinsTestsProject/JenkinsTestsProject/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace JenkinsTestsProject
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestsLogWord()
+        {
+            var word = "Hello World";
+            Console.WriteLine(word);
+            Assert.AreEqual("Hello World", word);
+        }
+    }
+}
